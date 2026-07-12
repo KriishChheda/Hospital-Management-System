c35456b6b1fcaeeafb73463e114d180087479f3b
KriishChheda <kriishchheda00522@gmail.com>
2026-07-12 17:04:09 +0530

    Vercel error

---
+++ src/app/(pharmacy)/prescriptionreports/page.tsx
@@ -0,0 +1,8 @@
+export default function PrescriptionReportsPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold">Prescription Reports</h1>
+            <p>This module is under construction.</p>
+        </div>
+    );
+}
